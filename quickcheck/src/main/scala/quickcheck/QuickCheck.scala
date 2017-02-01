5b1935a99aa69557817eef8a337c00d8e79bdd90
Daniel Bauman <baumand@amazon.com>
2017-02-01 23:27:51 +0200

    All working but bogus4

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -9,7 +9,12 @@ import Prop._
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
-  lazy val genHeap: Gen[H] = ???
+  lazy val genHeap: Gen[H] = for {
+    k <- arbitrary[Int]
+    z <- arbitrary[Int]
+    x <- arbitrary[Int]
+    m <- oneOf(empty, insert(1, empty))
+  } yield insert(x, insert(z, insert(k, m)))
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
   property("gen1") = forAll { (h: H) =>
@@ -17,4 +22,49 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(insert(m, h)) == m
   }
 
+  property("min1") = forAll { a: Int =>
+    val h = insert(a, empty)
+    findMin(h) == a
+  }
+
+  property("min2") = forAll { (a: Int, b: Int) =>
+    val h = insert(b, insert(a, empty))
+    findMin(h) == Math.min(a, b)
+  }
+
+  property("insdelmin") = forAll { a: Int =>
+    val h = insert(a, empty)
+    deleteMin(h) == empty
+  }
+
+  property("sortedwhenfindmin") = forAll { (h: H) =>
+    def sortedFrom(h1: H, min: Int): Boolean = {
+      if (h1 == empty) true else {
+        val heapMin = findMin(h1)
+        if (heapMin < min) false else sortedFrom(deleteMin(h1), heapMin)
+      }
+    }
+    val min = findMin(h)
+    sortedFrom(deleteMin(h), min)
+  }
+
+  property("minmeld") = forAll { (h1: H, h2: H) =>
+    def m = meld(h1, h2)
+    def minM = findMin(m)
+    minM == findMin(h1) | minM == findMin(h2)
+  }
+
+  property("doubleinsert") = forAll { (a: Int) =>
+    val a1 = if (a == Integer.MAX_VALUE) a - 1 else a
+    val a2 = a1 + 1
+    val heap = insert(a2, insert(a1, empty))
+    findMin(heap) == a1 & findMin(deleteMin(heap)) == a2
+  }
+
+  property("simpledoubleinsert") = {
+    val a1 = 5;
+    val a2 = a1 + 1
+    val heap = insert(a2, insert(a1, empty))
+    findMin(heap) == a1 & findMin(deleteMin(heap)) == a2
+  }
 }
